aa91953b0f162cc4d1531f08b4fc80c208a433fe
Héctor Álvarez Díaz <hector@eclipsedpixel.com>
2024-05-19 13:19:15 -0400

    Update connection to DB

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,6 @@
+use actix_service::configuration::get_configuration;
 use reqwest::Client;
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 
 // Lanzar nuestra aplicación en barckground -de alguna manera-
@@ -6,7 +8,7 @@ fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
 
     let port = listener.local_addr().unwrap().port();
-    let server = actix_service::run(listener).expect("Failed to bind address");
+    let server = actix_service::startup::run(listener).expect("Failed to bind address");
 
     let _ = tokio::spawn(server);
 
@@ -37,6 +39,14 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+
+    // The `Connection` trait MUST be in scope for us to invoke
+    // `PgConnection::connect` - is not an inherent method of the struct!
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect Postgres.");
 
     let client = Client::new();
 
@@ -52,6 +62,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request.");
 
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM suscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
